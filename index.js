d65d4b236f1b98294f8d3f7fe16468ce20775839
nahalewski <bennahalewski@me.com>
2025-01-26 18:43:35 -0500

    Fix static file serving and index.html routing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const path = require('path');
 
 const webApp = express();
 
@@ -7,16 +8,21 @@ const PORT = process.env.PORT || 5000;
 
 webApp.use(express.urlencoded({ extended: true }));
 webApp.use(express.json());
+webApp.use(express.static(path.join(__dirname, 'public')));
+
 webApp.use((req, res, next) => {
   console.log(`Path ${req.path} with Method ${req.method}`);
   next();
 });
 
-const homeRoute = require('./routes/homeRoute');
 const fbWebhookRoute = require('./routes/fbWebhookRoute');
 const sendMessageRoute = require('./routes/sendMessageRoute');
 
-webApp.use('/', homeRoute.router);
+// Serve index.html for both / and /index.html routes
+webApp.get(['/', '/index.html'], (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 webApp.use('/facebook', fbWebhookRoute.router);
 webApp.use('/sendMessage', sendMessageRoute.router);
 
